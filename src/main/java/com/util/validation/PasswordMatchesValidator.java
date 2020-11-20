a22d5288fa63dc41a1d818229432bbe7d9b7b805
oguzhanonder91 <oguzhanonder75@hotmail.com>
2020-11-20 22:41:32 +0300

    unused imports removed

--- src/main/java/com/util/validation/PasswordMatchesValidator.java
+++ src/main/java/com/util/validation/PasswordMatchesValidator.java
@@ -1,12 +1,12 @@
 package com.util.validation;
 
-import com.common.dto.PasswordDto;
-import com.common.dto.UserDto;
 import com.util.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 
 public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
@@ -23,16 +23,14 @@ public class PasswordMatchesValidator implements ConstraintValidator<PasswordMat
     public boolean isValid(Object obj, ConstraintValidatorContext constraintValidatorContext) {
         String password = null;
         String matchingPassword = null;
-        if (obj instanceof UserDto) {
-            UserDto user = (UserDto) obj;
-            password = user.getPassword();
-            matchingPassword = user.getMatchingPassword();
-        } else if (obj instanceof PasswordDto) {
-            PasswordDto passwordDto = (PasswordDto) obj;
-            password = passwordDto.getPassword();
-            matchingPassword = passwordDto.getMatchingPassword();
+        try {
+            Method getMethodPassword = obj.getClass().getDeclaredMethod("getPassword");
+            Method getMethodMatchingPassword = obj.getClass().getDeclaredMethod("getMatchingPassword");
+            password = (String) getMethodPassword.invoke(obj);
+            matchingPassword = (String) getMethodMatchingPassword.invoke(obj);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
         }
-
         return securityUtil.decode(password).equals(securityUtil.decode(matchingPassword));
     }
 }
